8c8dddee5428c6ac71bd5e435695db5a835c4ce2
Vitinho1 <vhsoaresbr@gmail.com>
2024-10-01 14:59:52 -0300

    Questões Java

--- Tt.java
+++ Tt.java
@@ -83,7 +83,7 @@ public class Quiz{
         perguntas[2].opcaoE = "E) Suco";
         perguntas[2].correta = "C";
 
-        perguntas[3] = new Questao;
+        perguntas[3] = new Questao();
         perguntas[3].pergunta = "Qual bebida é feita a partir da fermentação d milho?: ";
         perguntas[3].opcaoA = "A) Cerveja";
         perguntas[3].opcaoB = "B) Whisky";
@@ -92,7 +92,7 @@ public class Quiz{
         perguntas[3].opcaoE = "E) Chicha";
         perguntas[3].correta = "E";
 
-        perguntas[4] = new Questão;
+        perguntas[4] = new Questão();
         perguntas[4].pergunta = "Qual a principal bebida consumida na Alemanha?: ";
         perguntas[4].opcaoA = "A) Vodak";
         perguntas[4].opcaoB = "B) Vinho";
@@ -100,6 +100,15 @@ public class Quiz{
         perguntas[4].opcaoD = "D) Tequila";
         perguntas[4].opcaoE = "E) Champgne";
         perguntas[4].correta = "C";
+
+        perguntas[5] = new Questao();
+        perguntas[5].pergunta = "Qual é a bebida tradicional do Brasil feita a partir da cana-de-açúcar?: ";
+        perguntas[5].opcaoA = "A) Vodka";
+        perguntas[5].opcaoB = "B) Cerveja";
+        perguntas[5].opcaoC = "C) Vinho";
+        perguntas[5].opcaoD = "D) Cachaça";
+        perguntas[5].opcaoE = "E) Rum";
+        perguntas[5].correta = "D";
 
 
 
