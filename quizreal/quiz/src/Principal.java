52375bf3866475fd932637c70bb0a5221efb7a34
Vitinho1 <vhsoaresbr@gmail.com>
2024-11-25 16:02:47 -0300

    Alterando questões

--- quizreal/quiz/src/Principal.java
+++ quizreal/quiz/src/Principal.java
@@ -16,15 +16,14 @@ public class Principal {
 
         //proxima questao
 
-
         Questao q2 = new Questao();
-        q2.pergunta="1-quantos planetas existem no sistema solar?";
-        q2.opcaoA="a - 11";
-        q2.opcaoB="b - 12";
-        q2.opcaoC="c - 5";
-        q2.opcaoD="d - 7";
-        q2.opcaoE="e - 8";
-        q2.correta="e";
+        q2.pergunta = "2 - Quantos planetas existem no Sistema Solar?: ";
+        q2.opcaoA = "a - 11";
+        q2.opcaoB = "b - 12";
+        q2.opcaoC = "c - 5";
+        q2.opcaoD = "d - 7";
+        q2.opcaoE = "e - 8";
+        q2.correta = "e";
 
         q2.escrevaQuestao();
         resposta = q2.leiaResposta();
@@ -34,13 +33,13 @@ public class Principal {
 
 
         Questao q3 = new Questao();
-        q3.pergunta="1-quem descobriu o brasil?";
-        q3.opcaoA="a - pedro alvares cabral";
-        q3.opcaoB="b - jair messias bolsonaro";
-        q3.opcaoC="c - jucelino kubtchek";
-        q3.opcaoD="d - donald trump";
-        q3.opcaoE="e - bob chartlon";
-        q3.correta="a";
+        q3.pergunta = "3 - Qual a estrela presente no Sistema Solar?: ";
+        q3.opcaoA = "a - Lua";
+        q3.opcaoB = "b - Sol";
+        q3.opcaoC = "c - Buraco Negro";
+        q3.opcaoD = "d - Aurora Boreal";
+        q3.opcaoE = "e - BON 67";
+        q3.correta = "b";
 
         q3.escrevaQuestao();
         resposta = q3.leiaResposta();
@@ -50,13 +49,13 @@ public class Principal {
 
 
         Questao q4 = new Questao();
-        q4.pergunta="1-quantas estrelas existem no universo observável?";
-        q4.opcaoA="a - 1 bilhao 600 milhoes 934 mil";
-        q4.opcaoB="b - 23 milhoes e 500 mil ";
-        q4.opcaoC="c - 27 elevado a setima pontencia ";
-        q4.opcaoD="d - 498 milhoes ";
-        q4.opcaoE="e - 7 × 10 elevado a 22";
-        q4.correta="e";
+        q4.pergunta=" 4 - Quantas estrelas existem no Universo Observável?: ";
+        q4.opcaoA = "a - 1 bilhão 600 milhoes 934 mil";
+        q4.opcaoB = "b - 23 milhões e 500 mil ";
+        q4.opcaoC = "c - 27 elevado a sétima pontência ";
+        q4.opcaoD = "d - 498 milhões ";
+        q4.opcaoE = "e - 7 × 10 elevado a 22";
+        q4.correta = "e";
 
         q4.escrevaQuestao();
         resposta = q4.leiaResposta();
@@ -66,13 +65,13 @@ public class Principal {
 
 
         Questao q5 = new Questao();
-        q5.pergunta="1-quando aconteceu o atentado de 11 de setembro  ?";
-        q5.opcaoA="a - 14 de novembro";
-        q5.opcaoB="b - 11 de janeiro ";
-        q5.opcaoC="c - 32 de setembro  ";
-        q5.opcaoD="d - 11 de setembro ";
-        q5.opcaoE="e - ontem";
-        q5.correta="d";
+        q5.pergunta ="5 - Quais são os nomes corretos da fases da lua?: ";
+        q5.opcaoA = "a - Nova, Crescente, Cheia e Mingua.";
+        q5.opcaoB = "b - Velha, Decrescente, Vazia e Minguante.";
+        q5.opcaoC = "c - Nova, Crescente, Vazia e Míngua.";
+        q5.opcaoD = "d - Nova, Crescente, Cheia e Minguante.";
+        q5.opcaoE = "e - Velha, Crescente, Cheia e Minguante.";
+        q5.correta = "d";
 
         q5.escrevaQuestao();
         resposta = q5.leiaResposta();
