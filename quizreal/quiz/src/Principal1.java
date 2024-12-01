a96333aafc04009905ebcdfa51e0fc54028ac93b
Vitinho1 <vhsoaresbr@gmail.com>
2024-12-01 03:30:03 -0300

    Salvando últimas alterações

--- quizreal/quiz/src/Principal1.java
+++ quizreal/quiz/src/Principal1.java
@@ -1,61 +1,5 @@
-import java.util.Scanner;
-
-public class Questao {
-    String pergunta = "";
-    String opcaoA = "";
-    String opcaoB = "";
-    String opcaoC = "";
-    String opcaoD = "";
-    String opcaoE = "";
-    String correta = "";
-
-    public boolean isCorreta(String resposta) {
-        if (resposta.equalsIgnoreCase(this.correta)) {
-            System.out.println("Parabéns resposta Correta! - Letra: " + this.correta);
-            System.out.println("");
-            return true;
-        } else {
-            System.out.println("Resposta Errada!");
-            System.out.println("A opção correta é a letra: " + this.correta);
-            System.out.println("");
-            return false;
-        }
-    }
-
-    public String leiaResposta() {
-        String resp;
-        try (Scanner ler = new Scanner(System.in)) {
-            do {
-                System.out.println("Digite a resposta: ");
-                resp = ler.next();
-            } while (!respostaValida(resp));
-            return resp;
-        }
-    }
-
-    private boolean respostaValida(String resp) {
-        if (resp.equalsIgnoreCase("A") || resp.equalsIgnoreCase("B") || resp.equalsIgnoreCase("C") ||
-                resp.equalsIgnoreCase("D") || resp.equalsIgnoreCase("E")) {
-            return true;
-        }
-        System.out.println("Resposta inválida! Digite opção A, B, C, D ou E. ");
-        System.out.println("");
-        return false;
-    }
-
-    public void escrevaQuestao() {
-        System.out.println(this.pergunta);
-        System.out.println();
-        System.out.println(this.opcaoA);
-        System.out.println(this.opcaoB);
-        System.out.println(this.opcaoC);
-        System.out.println(this.opcaoD);
-        System.out.println(this.opcaoE);
-        System.out.println();
-    }
-
-    public class Quiz {
-        {
+ public class Principal1 {
+     public static void main(String[] args) {
 
             int acertos = 0;
             int erros = 0;
@@ -219,12 +163,15 @@ public class Questao {
             }
         }
 
-
         double porcentagem = (acertos / 15.0) * 100;
+
+
+        System.out.println("||===========================================================||");
 
         System.out.println("Você acertou " + acertos + " perguntas.");
         System.out.println("Você errou " + erros + " perguntas.");
         System.out.printf("Sua porcentagem de acertos é: %.2f%%\n", porcentagem);
+
+        System.out.println("||===========================================================||");
     }
-}
 }
